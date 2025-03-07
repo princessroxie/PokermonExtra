cccf344a1e200d9a16456648ba505590906e5ce9
princessroxie <awesome1998@gmail.com>
2025-03-07 11:43:53 -0500

    Update backs.lua

--- backs/backs.lua
+++ backs/backs.lua
@@ -3,7 +3,7 @@ local anabeldeck = {
 	key = "anabeldeck",  
     atlas = "backs",
     pos = { x = 0, y = 0 },
-	config = {jokers = {j_pokermon_snorlax},
+	config = {jokers = {j_poke_snorlax},
   loc_vars = function(self, info_queue, center)
     return {vars = {}}
   end
