511ac5657f95176893010e28031460ec2fc12499
princessroxie <awesome1998@gmail.com>
2025-03-07 11:45:40 -0500

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -8,10 +8,7 @@ return {
                         "Start with a ",
                         "{C:dark_edition}Snorlax{}"
                 }
-            },
-
-
-        }
+            }
 
         Sleeve = {
 
@@ -21,18 +18,7 @@ return {
                         "Start with a ",
                         "{C:dark_edition}Snorlax{}"
                 },
-            },
-
-            sleeve_sonfive_reverencesleeve_alt = {
-                name = "Reverence Sleeve",
-                text = {
-                        "Start with an ",
-                        "{C:dark_edition}Abra{}"
-                },
-            },
-
-
-
+            }
         }
     }
 }
