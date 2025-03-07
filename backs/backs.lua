9f9c379ef51c05b457563f12cf1969241ded4d95
princessroxie <awesome1998@gmail.com>
2025-03-07 11:39:47 -0500

    Update backs.lua

--- backs/backs.lua
+++ backs/backs.lua
@@ -2,7 +2,7 @@ local anabeldeck = {
 	name = "anabeldeck",
 	key = "anabeldeck",  
     atlas = "backs",
-    pos = { x = 1, y = 0 },
+    pos = { x = 0, y = 0 },
   loc_vars = function(self, info_queue, center)
     return {vars = {}}
   end
