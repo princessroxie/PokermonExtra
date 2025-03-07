9b3e54ea27e7d7a9bcb98914fc234ad43e3e7c79
princessroxie <awesome1998@gmail.com>
2025-03-07 10:42:13 -0500

    Update backs.lua

--- backs/backs.lua
+++ backs/backs.lua
@@ -1,65 +1,15 @@
-local reverencedeck = {
-    name = "reverencedeck",
-    key = "reverencedeck",
-    atlas = "backs",
-    pos = {x = 0, y = 0},
-    config = {},
-    loc_vars = function(self, info_queue, center)
-        return {vars = {}}
-    end,
-    apply = function(self)
-        G.GAME.modifiers.poke_force_seal = "poke_silver"
-    end
-}
-
-local virtuousdeck = {
+local anabeldeck = {
 	name = "virtuousdeck",
 	key = "virtuousdeck",  
     atlas = "backs",
     pos = { x = 1, y = 0 },
-	config = {consumables = {"c_sonfive_timerball"}, hands = -1, dollars = 0},
+	config = {jokers = {"j_snorlax"}},
   loc_vars = function(self, info_queue, center)
     return {vars = {}}
   end
 } 
 
-local propheticdeck ={
-    name = "propheticdeck",
-    key = "propheticdeck",
-    atlas = "backs",
-    pos = { x = 2, y = 0 },
-    config = {hand_size = -1, extra = {scry = 3}},
-    loc_vars = function(self, info_queue, center)
-        return {vars = {self.config.hand_size, self.config.extra.scry}}
-    end,
-
-    
-    apply = function(self)
-        G.GAME.scry_amount = self.config.extra.scry
-    end    
-}
-
-local shinydeck ={
-    name = "shinydeck",
-    key = "shinydeck",
-    atlas = "backs",
-    pos = { x = 3, y = 0 },
-    config = {extra = {chance = 100}},
-    loc_vars = function(self, info_queue, center)
-        return {vars = {}}
-    end,    
-
-    apply = function(self)
-        local previous_shiny_get_weight = G.P_CENTERS.e_poke_shiny.get_weight
-        G.P_CENTERS.e_poke_shiny.get_weight = function(self)
-          return previous_shiny_get_weight(self) + ((G.GAME.shiny_edition_rate or 1) - 1) * G.P_CENTERS.e_poke_shiny.weight
-        end
-        G.GAME.shiny_edition_rate = (G.GAME.shiny_edition_rate or 1) * self.config.extra.chance
-    end   
-}
-
-
-local dList = {reverencedeck, virtuousdeck, propheticdeck, shinydeck}
+local dList = {anabeldeck}
 
 return {name = "Back",
         list = dList
