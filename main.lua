e32378a8ccbaad79b5c8ca6a9e56c7099a622a61
princessroxie <awesome1998@gmail.com>
2025-03-07 12:00:10 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -46,29 +46,6 @@ if (SMODS.Mods["Pokermon"] or {}).can_load and SMODS.Mods["Pokermon"] and not po
     end
   end
 
-local pconsumables = NFS.getDirectoryItems(mod_dir.."consumables")
-
-if (SMODS.Mods["Pokermon"] or {}).can_load and SMODS.Mods["Pokermon"] then
-  for _, file in ipairs(pconsumables) do
-    sendDebugMessage ("The file is: "..file)
-    local consumable, load_error = SMODS.load_file("consumables/"..file)
-    if load_error then
-      sendDebugMessage ("The error is: "..load_error)
-    else
-      local curr_consumable = consumable()
-      if curr_consumable.init then curr_consumable:init() end
-      
-      for i, item in ipairs(curr_consumable.list) do
-        if ((not pokermon_config.jokers_only and not item.pokeball) or (item.pokeball and pokermon_config.pokeballs)) or (item.evo_item and not pokermon_config.no_evos) then
-          SMODS.Consumable(item)
-        end
-      end
-    end
-  end 
-end
-
-
-
 -- Get mod path and load other files
 mod_dir = ''..SMODS.current_mod.path
 if (SMODS.Mods["Pokermon"] or {}).can_load then
