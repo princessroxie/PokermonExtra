d2e62141314037a4ddb2adff4c6976911494747e
princessroxie <awesome1998@gmail.com>
2025-03-07 11:33:57 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,13 +13,6 @@ SMODS.Atlas({
 }):register()
 
 SMODS.Atlas({
-    key = "consumables",
-    path = "consumables.png",
-    px = 71,
-    py = 95
-}):register()
-
-SMODS.Atlas({
   key = "sleeves",
   px = 73,
   py = 95,
